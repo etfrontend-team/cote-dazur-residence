3e84ad9554064b84641b0fae263e7445cbaabb49
etfrontend-team <frontendteam.encircle@gmail.com>
2026-09-10 19:13:13 +0530

    design: contactus page design update

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,6 @@ import initTestimonialCard from "./testimonialCard.js";
 import initFadeIn from "./fadeIn.js";
 import initExploreCardReveal from "./exploreCardReveal.js";
 import initParallaxMedia from "./parallaxMedia.js";
-import initMapReveal from "./mapReveal.js";
 import initInnerHero from "./innerHero.js";
 import initAboutSection from "./aboutSection.js";
 import initCharReveal from "./charReveal.js";
@@ -52,7 +51,6 @@ window.addEventListener("DOMContentLoaded", () => {
   initFadeIn();
   initExploreCardReveal();
   initParallaxMedia();
-  initMapReveal();
   initInnerHero();
   initAboutSection();
   initCharReveal();
